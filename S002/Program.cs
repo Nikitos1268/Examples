913c53a04df1a833351be4409b5023fe809dbe9c
Nikita <nikitos1268@yandex.ru>
2023-04-02 21:01:09 +0300

    add S002

---
+++ S002/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int num = new Random().Next(10, 100);
+
+int num1 = num / 10;
+int num2 = num % 10;
+
+Console.WriteLine(num); 
+
+if (num1 > num2)
+{
+    Console.WriteLine($"num1 > num2 max = {num1} ");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"num1 < num2  max = {num2} ");
+}
+else
+{
+    Console.WriteLine($"num1 == num2  max = {num1} ");
+}
