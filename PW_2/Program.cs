f86e9b52bbc86a2d4d141108bda3b3963d0a8d70
Nikita <nikitos1268@yandex.ru>
2023-04-02 20:07:59 +0300

    rename

---
+++ PW_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Putt number day");
+string input = Console.ReadLine();
+switch (Convert.ToInt32(input))
+{
+    case 1:
+        Console.WriteLine("понедельник");
+        break; 
+    case 2:
+        Console.WriteLine("вторник");
+        break; 
+    case 3:
+        Console.WriteLine("среда");
+        break; 
+    case 4:
+        Console.WriteLine("четверг");
+        break; 
+    case 5:
+        Console.WriteLine("пятница");
+        break; 
+    case 6:
+        Console.WriteLine("суббота");
+        break; 
+    case 7:
+        Console.WriteLine("воскресенье");
+        break;
+}
